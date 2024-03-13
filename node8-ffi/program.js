4c28ef878e38378204350f2d00ed089bd564bf5c
cabbage <gaof2001@hotmail.com>
2024-03-13 00:40:25 -0700

    Replace ffi to ffi-napi to adapt node16

--- node8-ffi/program.js
+++ node8-ffi/program.js
@@ -1,7 +1,7 @@
 'use strict'
 
-var ref = require("ref");
-var ffi = require("ffi");
+var ref = require("ref-napi");
+var ffi = require("ffi-napi");
 
 //第一个形参为dll所在位置（dll文件可以不用加.dll），第二个为函数信息
 console.log("__dirname:", __dirname)
@@ -12,5 +12,5 @@ var libm = ffi.Library(__dirname + '/bin/Win32Dll1.dll', {
 
 //调用
 var str1 = "acc";
-var str2 = "bionology,,,,";
+var str2 = "bionology,,,,bone";
 console.log("Result from dll:", libm.fun(str1, str2));
